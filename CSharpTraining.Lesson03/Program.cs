b2db4e5415d3c677010db52280c73bcf0b7c4d50
Jason Tennant <xcodula@icloud.com>
2017-04-24 13:09:22 -0700

    updates

---
+++ CSharpTraining.Lesson03/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    static void Main()
+    {
+        var addition = 1 + 2;
+        System.Console.WriteLine($"1 + 2 = {addition}");
+
+        var subtraction = 3 - 2;
+        System.Console.WriteLine($"3 - 2 = {subtraction}");
+
+        var multiplication = 2 * 4;
+        System.Console.WriteLine($"2 * 4 = {multiplication}");
+
+        var division = 5 / 2;
+        System.Console.WriteLine($"5 / 2 = {division}");
+
+        var modulo = 5 % 2;
+        System.Console.WriteLine($"5 % 2 = {modulo}");
+
+        addition++;
+        System.Console.WriteLine($"addition++ = {addition}");
+
+        subtraction--;
+        System.Console.WriteLine($"subtraction-- = {subtraction}");
+
+        //var division = 5.0 / 2.0;
+        //System.Console.WriteLine($"5.0 / 2.0 = {division}");
+
+        // This line will prevent the console window from closing immediately.
+        System.Console.ReadLine();
+    }
+}
